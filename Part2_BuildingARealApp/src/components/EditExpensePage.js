266f4f934504f9fa69c5703b71e4ce20ea5005fd
Adam <avvallky@gmail.com>
2021-08-08 21:15:50 -0700

    Modifying routes to reflect edit for specific records.

--- Part2_BuildingARealApp/src/components/EditExpensePage.js
+++ Part2_BuildingARealApp/src/components/EditExpensePage.js
@@ -1,9 +1,25 @@
 import React from 'react';
 
-const EditExpensePage = () => (
-    <div>
-        <p>This is from my edit expense component.</p>
-    </div>
-);
+const EditExpensePage = (props) => {
+    console.log(props);
+    return (
+        <div>
+            {props.match.params.id ? 
+                <p>
+                    Edit the expense of record with id {props.match.params.id}
+                </p> : 
+                <p>
+                    No record provided to edit.
+                </p>
+            }
+            {/* {props.match.params ? 
+                (props.match.params.id ? 
+                    (<p>Edit the expense of record with id {props.match.params.id}</p>) :
+                    (<p>Component does not exist.</p>)
+                ) : (<p>Component does not exist.</p>)
+            } */}
+        </div>
+    );
+};
 
 export default EditExpensePage;
--- Part2_BuildingARealApp/src/routers/AppRouter.js
+++ Part2_BuildingARealApp/src/routers/AppRouter.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from '../components/Header';
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
 import AddExpensePage from '../components/AddExpensePage';
-import EditExpensePage from '../components/ExpenseDashboardPage';
+import EditExpensePage from '../components/EditExpensePage';
 import HelpPage from '../components/HelpPage';
 import NotFoundPage from '../components/NotFoundPage';
 
@@ -12,11 +12,11 @@ const AppRouter = () => (
         <div>
             <Header />
             <Switch>
-                <Route path="/" component={ExpenseDashboardPage} exact={true}></Route>
-                <Route path="/create" component={AddExpensePage} exact={true}></Route>
-                <Route path="/edit" component={EditExpensePage} exact={true}></Route>
-                <Route path="/help" component={HelpPage} exact={true}></Route>
-                <Route component={NotFoundPage}></Route>
+                <Route path="/" component={ExpenseDashboardPage} exact={true} />
+                <Route path="/create" component={AddExpensePage} exact={true} />
+                <Route path="/edit/:id" component={EditExpensePage} />
+                <Route path="/help" component={HelpPage} exact={true} />
+                <Route component={NotFoundPage} />
             </Switch>
         </div>
     </BrowserRouter>
